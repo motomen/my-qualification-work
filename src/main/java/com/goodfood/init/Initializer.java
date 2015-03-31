ca7e0b25e395c4ab6ee7926a0e4eaf766e69cdaa
motomen <gdpdp.ru@mail.ru>
2015-03-31 20:03:56 +0300

    delete xml config

--- src/main/java/com/goodfood/init/Initializer.java
+++ src/main/java/com/goodfood/init/Initializer.java
@@ -1,6 +1,11 @@
 package com.goodfood.init;
 
+import javax.servlet.Filter;
+
 import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Order(1)
@@ -22,4 +27,14 @@ public class Initializer extends
 		return new String[] { "/" };
 	}
 
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		encodingFilter.setForceEncoding(true);
+
+		DelegatingFilterProxy reconnectDelegate = new DelegatingFilterProxy("apiExceptionHandler");
+
+		return new Filter[] { reconnectDelegate, encodingFilter, new HiddenHttpMethodFilter() };
+	}
 }
