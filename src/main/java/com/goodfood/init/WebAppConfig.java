c7cb6c2d25d352980fb0719296f8479dd8e6af6e
motomen <gdpdp.ru@mail.ru>
2015-04-03 17:02:01 +0300

    finish config apache tiles

--- src/main/java/com/goodfood/init/WebAppConfig.java
+++ src/main/java/com/goodfood/init/WebAppConfig.java
@@ -1,10 +1,5 @@
 package com.goodfood.init;
 
-import org.apache.tiles.Attribute;
-import org.apache.tiles.Definition;
-import org.apache.tiles.context.TilesRequestContextHolder;
-import org.apache.tiles.definition.UnresolvingLocaleDefinitionsFactory;
-import org.apache.tiles.request.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,71 +8,31 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.goodfood.controller")
 public class WebAppConfig  extends WebMvcConfigurerAdapter{
 
-	private static final Map<String, Definition> tiles = new HashMap<String,Definition>();
-	private static final Attribute TEMPLATE = new Attribute("/WEB-INF/views/layout/layout.jsp");
 
 	@Bean
-	public TilesConfigurer tilesConfigurer() {
-		TilesConfigurer tilesConfigurer = new TilesConfigurer();
-		tilesConfigurer.setDefinitionsFactoryClass(JavaDefinitionsFactory.class);
-		tilesConfigurer.setDefinitions(new String[] {});
-
-		addDefinition("home", "Home", "/WEB-INF/views/home.jsp");
-		addDefinition("signin", "Sign in", "/WEB-INF/views/signin.jsp");
-		addDefinition("signin/facebook", "Sign in", "/WEB-INF/views/signin.jsp");
-
-		return tilesConfigurer;
-	}
-
-	private void addDefinition(String name, String title, String body) {
-		Map<String, Attribute> attributes = getDefaultAttributes();
-
-		attributes.put("title", new Attribute(title));
-		attributes.put("body", new Attribute(body));
-
-		tiles.put(name, new Definition(name, TEMPLATE, attributes));
-	}
-
-	private Map<String, Attribute> getDefaultAttributes() {
-		Map<String, Attribute> attributes = new HashMap<String,Attribute>();
-
-		attributes.put("header", new Attribute("/WEB-INF/views/layout/header.jsp"));
-		attributes.put("menu", new Attribute("/WEB-INF/views/layout/menu.jsp"));
-		attributes.put("footer", new Attribute("/WEB-INF/views/layout/footer.jsp"));
-
-		return attributes;
-	}
-
-	@Bean
-	public ViewResolver viewResolver() {
+	public UrlBasedViewResolver viewResolver() {
 		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
 		viewResolver.setViewClass(TilesView.class);
 		return viewResolver;
 	}
 
-	public static class JavaDefinitionsFactory extends UnresolvingLocaleDefinitionsFactory {
-
-		public JavaDefinitionsFactory(){}
-
-		@Override
-		public Definition getDefinition(String name, Request tilesContext) {
-			return tiles.get(name);
-		}
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
+		tilesConfigurer.setDefinitions(new String[] { "/WEB-INF/tiles.xml" });
+		tilesConfigurer.setCheckRefresh(true);
+		return tilesConfigurer;
 	}
 
 	// spring message config bean
