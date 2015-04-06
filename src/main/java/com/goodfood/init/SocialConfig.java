96807a12f6fcf0c075da2f1974131aefa6de5745
motomen <gdpdp.ru@mail.ru>
2015-04-06 08:44:17 +0300

    google+ config connection

--- src/main/java/com/goodfood/init/SocialConfig.java
+++ src/main/java/com/goodfood/init/SocialConfig.java
@@ -1,6 +1,9 @@
 package com.goodfood.init;
 
+import com.goodfood.model.User;
 import com.goodfood.social.PostToWallAfterConnectInterceptor;
+import com.goodfood.social.user.SecurityContext;
+import com.goodfood.social.user.SimpleConnectionSignUp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -18,11 +21,14 @@ import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
 import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.connect.web.ReconnectFilter;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.facebook.web.DisconnectController;
+import org.springframework.social.google.api.Google;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -38,11 +44,17 @@ public class SocialConfig implements SocialConfigurer {
     @Inject
     private DataSource dataSource;
 
+    @Inject
+    private Environment environment;
+
     @Override
     public void addConnectionFactories(ConnectionFactoryConfigurer cfConfig, Environment env) {
         cfConfig.addConnectionFactory(new FacebookConnectionFactory(
                 env.getProperty("facebook.app.id"),
                 env.getProperty("facebook.app.secret")));
+        cfConfig.addConnectionFactory(new GoogleConnectionFactory(
+                env.getProperty("googlep.app.id"),
+                env.getProperty("googlep.app.secret")));
     }
 
     /**
@@ -91,5 +103,48 @@ public class SocialConfig implements SocialConfigurer {
     public Facebook facebook(ConnectionRepository repository) {
         Connection<Facebook> connection = repository.findPrimaryConnection(Facebook.class);
         return connection != null ? connection.getApi() : null;
+    }
+
+    /**
+     * A proxy to a request-scoped object representing the current user's primary Facebook account.
+     * @throws  if the user is not connected to facebook.
+     */
+    @Bean
+    @Scope(value="request", proxyMode=ScopedProxyMode.INTERFACES)
+    public Google google() {
+        return connectionRepository().getPrimaryConnection(Google.class).getApi();
+    }
+
+    /**
+     * Request-scoped data access object providing access to the current user's connections.
+     */
+    @Bean
+    @Scope(value="request", proxyMode=ScopedProxyMode.INTERFACES)
+    public ConnectionRepository connectionRepository() {
+        User user = SecurityContext.getCurrentUser();
+        return usersConnectionRepository().createConnectionRepository(user.getIdUserStr());
+    }
+
+    /**
+     * Singleton data access object providing access to connections across all users.
+     */
+    @Bean
+    public UsersConnectionRepository usersConnectionRepository() {
+        JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource,
+                connectionFactoryLocator(), Encryptors.noOpText());
+        repository.setConnectionSignUp(new SimpleConnectionSignUp());
+        return repository;
+    }
+
+    /**
+     * When a new provider is added to the app, register its {@link } here.
+     * @see GoogleConnectionFactory
+     */
+    @Bean
+    public ConnectionFactoryLocator connectionFactoryLocator() {
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+        registry.addConnectionFactory(new GoogleConnectionFactory(environment.getProperty("googlep.app.secret"),
+                environment.getProperty("googlep.app.secret")));
+        return registry;
     }
 }
--- src/main/webapp/WEB-INF/template/login/body.jsp
+++ src/main/webapp/WEB-INF/template/login/body.jsp
@@ -24,6 +24,19 @@
             <p><button type="submit"></button></p>
             <label for="postToWall"><input id="postToWall" type="checkbox" name="postToWall" /> Tell your friends about Spring Social Showcase on your Facebook wall</label>
         </form>
+        <div class="container">
+            <div class="row">
+                <div class="span14 columns offset2">
+                    <h1>Spring Social Google Example Application</h1>
+                    <form action="signin/google" method="POST">
+                        <button type="submit" class="btn btn-large btn-primary">Sign in with Google</button>
+                        <input type="hidden" name="scope" value="email https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/latitude.all.best" />
+                        <input type="hidden" name="request_visible_actions" value="http://schemas.google.com/AddActivity http://schemas.google.com/BuyActivity http://schemas.google.com/CheckInActivity http://schemas.google.com/CommentActivity http://schemas.google.com/CreateActivity http://schemas.google.com/DiscoverActivity http://schemas.google.com/ListenActivity http://schemas.google.com/ReserveActivity http://schemas.google.com/ReviewActivity http://schemas.google.com/WantActivity"/>
+                        <input type="hidden" name="access_type" value="offline"/>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
